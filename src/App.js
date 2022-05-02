e4ca6c61c7119ab0a4858cb6bd118434a1519deb
Bikram Mondal <bikramm8617@gmail.com>
2022-05-02 11:24:42 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import './App.css';
 import { useSelector, useDispatch } from 'react-redux';
-import { incAction, decAction } from './actions/index';
+import { incAction, decAction } from './actions/actions';
 
 function App() {
-  const myState = useSelector(state => state.changeTheNumber)
+  const myState = useSelector((state) => state.changeTheNumber);
   const dispatch = useDispatch();
   return (
     <>
-        <div className=" mt-5 p-3 container text-center">
-          <h2> Welcome to React Redux</h2>
-          <h3>Increment & Decrement Counter</h3>
-          <div>
-            <button className="decrement btn btn-danger mx-2 text-white" onClick={() => dispatch(decAction())}><span> - </span></button>
-            <input className="text-center" type="text" name="quantity" value={myState} readOnly/>
-            <button className="increment btn btn-success mx-2 text-white" onClick={() => dispatch(incAction(5))} ><span> + </span></button>
-          </div>
+      <div className=" mt-5 p-3 container text-center">
+        <h2> Welcome to React Redux</h2>
+        <h3>Increment & Decrement Counter</h3>
+        <div>
+          <button
+            className="decrement btn btn-danger mx-2 text-white"
+            onClick={() => dispatch(decAction())}
+          >
+            <span> - </span>
+          </button>
+          <input
+            className="text-center"
+            type="text"
+            name="quantity"
+            value={myState}
+            readOnly
+          />
+          <button
+            className="increment btn btn-success mx-2 text-white"
+            onClick={() => dispatch(incAction(10))}
+          >
+            <span> + </span>
+          </button>
         </div>
+      </div>
     </>
   );
 }
